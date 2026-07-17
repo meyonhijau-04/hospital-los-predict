4d643efb3784433615d69108f15cd887711e6f2f
meyonhijau-04 <meyonhijau@gmail.com>
2026-07-17 13:51:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -369,7 +369,6 @@ def about():
 # ============================================================
 # MAIN
 # ============================================================
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    print(f"[OK] Server berjalan di http://localhost:{port}")
-    app.run(host="0.0.0.0", port=port, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
